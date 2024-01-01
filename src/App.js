9c534f01e9ada0a2db15b297f0e409bce72b9877
Bruno Costa <brunohc1991@gmail.com>
2024-01-01 15:26:24 -0300

    work with useEffect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,45 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
-import { useState } from 'react';
+
+const eventfn = () => {
+  console.log('h1 clicado');
+};
 
 function App() {
-  const [reverse, setReverse] = useState(false);
-  const [counter, setCounter] = useState(0);
-  const reverseClass = reverse ? 'reverse' : '';
+  // const [reverse, setReverse] = useState(false);
+  // const [counter, setCounter] = useState(0);
+  // const reverseClass = reverse ? 'reverse' : '';
 
-  const handleClick = () => {
-    setReverse(!reverse);
-    setCounter(counter + 1);
-  };
+  // const handleClick = () => {
+  //   setReverse(!reverse);
+  //   setCounter(counter + 1);
+  // };
+
+  const [counter, setCounter] = useState(0);
+
+  //component did update = Executa toda vez que o componente atualiza
+  useEffect(() => {
+    console.log('component did update');
+  });
+
+  //component did mount = Executa uma vez
+  useEffect(() => {
+    console.log('component did mount');
+    document.querySelector('h1')?.addEventListener('click', eventfn);
+
+    return () => {
+      document.querySelector('h1')?.removeEventListener('click', eventfn);
+    };
+  }, []);
+
+  //component did mount com dependencia = Executa toda vez q a dependencia mudar
+  useEffect(() => {
+    console.log('component did mount with dependence');
+  }, [counter]);
 
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -23,7 +48,9 @@ function App() {
         <button type="button" onClick={handleClick}>
           Reverse
         </button>
-      </header>
+      </header> */}
+      <h1>Contador: {counter}</h1>
+      <button onClick={() => setCounter(counter + 1)}>Somar</button>
     </div>
   );
 }
