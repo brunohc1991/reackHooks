9d89648d8dd469b7849c9618e7dfbb1cb4d81371
Bruno Costa <brunohc1991@gmail.com>
2024-01-02 06:24:48 -0300

    useMemo example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, useMemo } from 'react';
 import './App.css';
 import P from 'prop-types';
 
-const Button = React.memo(function Button({ incrementCounter }) {
-  return <button onClick={() => incrementCounter(10)}>+</button>;
-});
+const Post = ({ post }) => {
+  console.log('Filho renderizou');
+  return (
+    <div key={post.id} className="post">
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+    </div>
+  );
+};
 
-Button.propTypes = {
-  incrementCounter: P.func,
+Post.propTypes = {
+  post: P.shape({
+    id: P.number,
+    title: P.string,
+    body: P.string,
+  }),
 };
 
 function App() {
-  const [counter, setCounter] = useState(0);
+  console.log('Pai renderizou');
+  const [posts, setPosts] = useState([]);
+  const [value, setValue] = useState('');
 
-  const incrementCounter = useCallback((num = 1) => {
-    setCounter((c) => c + num);
+  //Component did mount
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then((response) => response.json())
+      .then((response) => setPosts(response));
   }, []);
-
   return (
     <div className="App">
-      <h1>Contador: {counter}</h1>
-      <Button incrementCounter={incrementCounter} />
+      <p>
+        <input
+          type="search"
+          value={value}
+          onChange={(e) => {
+            setValue(e.target.value);
+          }}
+        />
+      </p>
+      {useMemo(() => {
+        return posts.map((post) => <Post key={post.id} post={post} />);
+      }, [posts])}
     </div>
   );
 }
