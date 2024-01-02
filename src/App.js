3b23ac8df06a9313018a2ba43ae7979f62284df7
Bruno Costa <brunohc1991@gmail.com>
2024-01-02 05:47:50 -0300

    useCallback example

--- src/App.js
+++ src/App.js
@@ -1,56 +1,26 @@
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import './App.css';
+import P from 'prop-types';
 
-const eventfn = () => {
-  console.log('h1 clicado');
+const Button = React.memo(function Button({ incrementCounter }) {
+  return <button onClick={() => incrementCounter(10)}>+</button>;
+});
+
+Button.propTypes = {
+  incrementCounter: P.func,
 };
 
 function App() {
-  // const [reverse, setReverse] = useState(false);
-  // const [counter, setCounter] = useState(0);
-  // const reverseClass = reverse ? 'reverse' : '';
-
-  // const handleClick = () => {
-  //   setReverse(!reverse);
-  //   setCounter(counter + 1);
-  // };
-
   const [counter, setCounter] = useState(0);
 
-  //component did update = Executa toda vez que o componente atualiza
-  useEffect(() => {
-    console.log('component did update');
-  });
-
-  //component did mount = Executa uma vez
-  useEffect(() => {
-    console.log('component did mount');
-    document.querySelector('h1')?.addEventListener('click', eventfn);
-
-    return () => {
-      document.querySelector('h1')?.removeEventListener('click', eventfn);
-    };
+  const incrementCounter = useCallback((num = 1) => {
+    setCounter((c) => c + num);
   }, []);
-
-  //component did mount com dependencia = Executa toda vez q a dependencia mudar
-  useEffect(() => {
-    console.log('component did mount with dependence');
-  }, [counter]);
 
   return (
     <div className="App">
-      {/* <header className="App-header">
-        <img src={logo} className={`App-logo ${reverseClass}`} alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h1>Contador: {counter}</h1>
-        <button type="button" onClick={handleClick}>
-          Reverse
-        </button>
-      </header> */}
       <h1>Contador: {counter}</h1>
-      <button onClick={() => setCounter(counter + 1)}>Somar</button>
+      <Button incrementCounter={incrementCounter} />
     </div>
   );
 }
